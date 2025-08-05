8224a2185b7b3dd63edc6e90030d95de570de1ba
twinlight <info@twinlight.jp>
2025-08-05 16:34:24 +0900

    _LINE Key

--- index.js
+++ index.js
@@ -57,8 +57,8 @@ const { Client, middleware } = require('@line/bot-sdk');
 require('dotenv').config();
 
 const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
 
 const client = new Client(config);
